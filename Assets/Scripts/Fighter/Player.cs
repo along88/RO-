a82d2a857ce053b0da90ef123fd7751ff6c9f4ce
along88 <a.long88@yahoo.com>
2018-09-20 19:03:00 -0400

    no message

---
+++ Assets/Scripts/Fighter/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Fighter fighter;
+
+    private Rigidbody rb;
+    private Quaternion newRotation;
+
+    public  Fighter Fighter
+    {
+        get { return fighter; }
+        set { fighter = value; }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        newRotation = new Quaternion(0.0f, -135.0f, 0.0f, 0.0f);
+    }
+
+    private void Update()
+    {
+        Move(Input.GetAxisRaw("Horizontal1"), Input.GetAxisRaw("Vertical1"));
+       
+    }
+
+    private void Move(float x, float z)
+    {
+        Vector3 moveDirection = new Vector3(x * Fighter.Speed, 0.0f, z * Fighter.Speed) * Time.deltaTime;
+
+        rb.velocity = moveDirection;
+
+        Rotate(moveDirection);
+       
+        
+    }
+    private void Rotate(Vector3 direction)
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction),5.0f * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/scriptableObject/Fighters/Fighter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Fighter : ScriptableObject
+{
+    [SerializeField]
+    private string fighterName;
+
+    [SerializeField]
+    private float health;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float damage;
+
+    public string FighterName { get { return fighterName; } set { fighterName = value; } }
+
+    public float Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+    public float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+    public float Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+}
