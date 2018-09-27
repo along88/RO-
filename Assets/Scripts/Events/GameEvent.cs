58f8f7238831e683f63425214f05bcc99320cb01
joshmond <joshmnd8@gmail.com>
2018-09-27 20:34:16 +0100

    Implemented basic movement and created an event system that exposes the events/responses to the inspector for more designer-friendly control.

---
+++ Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Event", menuName = "Events/Event", order = 1)]
+public class GameEvent : ScriptableObject
+{
+    private List<EventListener> listeners = new List<EventListener>();
+
+    public void Raise()
+    {
+        for (int index = listeners.Count - 1; index >= 0; index--)
+        {
+            listeners[index].OnNotify();
+        }
+    }
+
+    public void RegisterEvent(EventListener listener)
+    {
+        if (!listeners.Contains(listener))
+        {
+            listeners.Add(listener);
+        }
+    }
+
+    public void RemoveEvent(EventListener listener)
+    {
+        if (listeners.Contains(listener))
+        {
+            listeners.Remove(listener);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 movement;
+
+    [SerializeField]
+    private GameEvent gameEvent;
+
+    void Awake()
+    {
+        
+    }
+
+    void Start ()
+    {
+        
+	}
+
+	void Update ()
+    {
+        Move(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameEvent.Raise();
+        }
+
+
+    }
+
+    private void Move(float xPos, float zPos)
+    {
+        movement = new Vector3(xPos, 0.0f, zPos);
+        transform.Translate(movement * 20.0f * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 20.0f);
+    }
+
+    public void OnHit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+   
+
+
+}
