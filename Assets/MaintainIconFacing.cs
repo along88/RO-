de026a8212438b44aff8245e67bdae08cad36d1b
HNIC <a.long88@yahoo.com>
2019-10-24 15:45:34 -0400

    Player Icons are now maintaining their facing however a issue still needs to be resolved when P1 selects Dukez or P2 Selects Marie, the icons are reversed when this happens.

---
+++ Assets/MaintainIconFacing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainIconFacing : MonoBehaviour
+{
+    public Quaternion facing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        facing = this.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //maintain facing at all times here
+        this.transform.rotation = facing;
+    }
+}
--- Assets/_SCRIPTS/Archived/11.29.17/Managers/MatchManager.cs
+++ Assets/_SCRIPTS/Archived/11.29.17/Managers/MatchManager.cs
@@ -78,17 +78,14 @@ using UnityEngine.UI;
             {
                 case global::Fighters.MARIE:
                 FighterModel[0] = Instantiate(prefabs[0], new Vector3(-39, 43, 10), Quaternion.LookRotation(Vector3.forward));
-                FighterModel[0].GetComponentInChildren<SpriteRenderer>().sprite = PlayerIcon[0];
+                //FighterModel[0].GetComponentInChildren<SpriteRenderer>().sprite = PlayerIcon[0];
                 break;
-            case global::Fighters.DUKEZ:
-                if (fighters[0] == fighters[1] && !priority[0])
-                {
+                case global::Fighters.DUKEZ:
+                
                     FighterModel[0] = Instantiate(prefabs[1], new Vector3(-39, 43, 10), Quaternion.LookRotation(Vector3.forward));
-                    //FighterModel[0].GetComponent<Image>().material.color = Color.red;
-                }
-                else
-                    FighterModel[0] = Instantiate(prefabs[1], new Vector3(-39, 43, 10), Quaternion.LookRotation(Vector3.forward));
+                    FighterModel[0].GetComponentInChildren<MaintainIconFacing>().facing = new Quaternion(0, 0, 0, 1f);
 
+                
                 break;
             default:
                 break;
@@ -100,8 +97,10 @@ using UnityEngine.UI;
                 {
                     //fighters are the same, make p2 different color
                     FighterModel[1] = Instantiate(prefabs[0], new Vector3(9, 43, 10), Quaternion.LookRotation(Vector3.back));
+                    var IconRotation = FighterModel[1].GetComponentInChildren<MaintainIconFacing>().facing;
+                    FighterModel[1].GetComponentInChildren<MaintainIconFacing>().facing = new Quaternion(0, IconRotation.y, 0, 1f);
 
-                   // FighterModel[1].GetComponent<Image>().material.color = Color.red;
+                    // FighterModel[1].GetComponent<Image>().material.color = Color.red;
                 }
                 else
                     FighterModel[1] = Instantiate(prefabs[0], new Vector3(9, 43, 10), Quaternion.LookRotation(Vector3.back));
